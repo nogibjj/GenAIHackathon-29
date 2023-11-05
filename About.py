f1969f1f308b585cd93e3185547dd00127e9ca74
jeremymtan <jemtan@ucsc.edu>
2023-11-05 03:36:34 -0500

    add landing page

---
+++ About.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Insert name Here! 👋")
+
+
+st.markdown(
+    """
+    Our motivation for creating an AI chatbot that serves as our chief, recipe generator, and meal planner stems from our desire for convenient, personalized, and enjoyable cooking experiences. By providing the chatbot with our dietary requirements, available ingredients, and time constraints, we aim to receive tailored recipe suggestions that suit our needs and preferences, enhancing our daily meals. Additionally, the chatbot's capability to recommend nearby supermarkets and grocery options based on our information not only simplifies the shopping process but also promotes healthy and efficient meal planning. Looking ahead, we envision the chatbot evolving to assist with meal delivery or pickup orders, further streamlining our culinary journey and making cooking a delightful and stress-free endeavor.
+    """
+)
