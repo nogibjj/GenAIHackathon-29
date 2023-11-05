0769648fa73421618a515e40fb1ba8ed7eaa4321
jeremymtan <jemtan@ucsc.edu>
2023-11-05 01:43:12 -0500

    added the columns

--- app.py
+++ app.py
@@ -2,14 +2,54 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 import openai
+import pandas as pd
+import googlemaps
+from streamlit_js_eval import get_geolocation
+import folium
+from streamlit_folium import st_folium
 
 load_dotenv()
 
 # Set up your OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 # Initialize conversation history as an empty list
 conversation_history = []
+
+
+def miles_to_meter(miles):
+    """miles to meter conversion"""
+    return miles * 1_609.344
+
+
+def dalle(input_promt):
+    """gets an image with openai"""
+    response = openai.Image.create(prompt=input_promt, n=1, size="256x256")
+    return response["data"][0]["url"]
+
+
+def get_supermarkets(loc, search_string="supermarket", distance=miles_to_meter(2)):
+    """grabs nearest supermakets"""
+    business_list = []
+    map_client = googlemaps.Client(GOOGLE_API_KEY)
+    # print(loc)
+    response = map_client.places_nearby(
+        location=loc, keyword=search_string, name="supermarket", radius=distance
+    )
+    # print("here")
+    business_list.extend(response.get("results"))
+    # next_page_token = response.get('next_page_token')
+
+    df = pd.DataFrame(business_list)
+    df["url"] = "http://www.google.com/maps/place/?q=place_id:" + df["place_id"]
+    df.sort_values("rating", inplace=True, ascending=False)
+    lat, lng = (
+        df["geometry"][1]["location"]["lat"],
+        df["geometry"][1]["location"]["lng"],
+    )
+    print(lat, lng)
+    return lat, lng
 
 
 def get_completion(messages, model="gpt-3.5-turbo"):
@@ -28,6 +68,8 @@ st.title("Figure out a good name")
 
 # Get user input using a text input field
 text = st.text_input("Enter your prompt here:")
+location = get_geolocation()
+
 
 # Check if the user has entered a prompt
 if text:
@@ -58,21 +100,40 @@ if text:
     conversation_history.append({"role": "assistant", "content": response})
     # Display the response in the desired format
     formatted_response = response.split("\n")
+    # Get the image URL using the 'dalle' function
+    image_url = dalle(response)
 
     # Create three columns
-    col1, col2, col3 = st.columns(3)
+    if location:
+        col1, col2, col3 = st.columns(3)
 
-    # Column 1 - Placeholder for some content
-    with col1:
-        st.write("Column 1 Content")
-        # You can add any content you want in this column
+        # Column 1 - Placeholder for some content
+        with col1:
+            # Display the image
+            st.image(image_url, use_column_width=True)
 
-    # Column 2 - Placeholder for the response from the prompt
-    with col2:
-        for line in formatted_response:
-            st.markdown(line)
+        # Column 2 - Placeholder for the response from the prompt
+        with col2:
+            for line in formatted_response:
+                st.markdown(line)
 
-    # Column 3 - Placeholder for some content
-    with col3:
-        st.write("Column 3 Content")
-        # You can add any content you want in this column
+        # Column 3 - Placeholder for some content
+        with col3:
+            print(location)
+            lat, long = location["coords"]["latitude"], location["coords"]["longitude"]
+            lat, lng = get_supermarkets((lat, long))
+            st.write("Best Supermarket")
+            m = folium.Map(location=[lat, lng], zoom_start=16)
+            st_data = st_folium(m, width=725)
+            # You can add any content you want in this column
+    else:
+        col1, col2 = st.columns(2)
+        # Column 1 - Placeholder for some content
+        with col1:
+            # Display the image
+            st.image(image_url, use_column_width=True)
+
+        # Column 2 - Placeholder for the response from the prompt
+        with col2:
+            for line in formatted_response:
+                st.markdown(line)
