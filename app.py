714d308d743b53d52d6ad71381235f649d8695fe
jeremymtan <jemtan@ucsc.edu>
2023-11-05 03:21:07 -0500

    add fixes and download

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import googlemaps
 from streamlit_js_eval import get_geolocation
 import folium
 from streamlit_folium import st_folium
+import streamlit_ext as ste
 
 load_dotenv()
 
@@ -14,8 +15,14 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-# Initialize conversation history as an empty list
-conversation_history = []
+if "conversation_history" not in st.session_state:
+    st.session_state.conversation_history = []
+
+if "text" not in st.session_state:
+    st.session_state.text = ""
+
+if "location" not in st.session_state:
+    st.session_state.location = None
 
 
 def miles_to_meter(miles):
@@ -33,13 +40,10 @@ def get_supermarkets(loc, search_string="supermarket", distance=miles_to_meter(2
     """grabs nearest supermakets"""
     business_list = []
     map_client = googlemaps.Client(GOOGLE_API_KEY)
-    # print(loc)
     response = map_client.places_nearby(
         location=loc, keyword=search_string, name="supermarket", radius=distance
     )
-    # print("here")
     business_list.extend(response.get("results"))
-    # next_page_token = response.get('next_page_token')
 
     df = pd.DataFrame(business_list)
     df["url"] = "http://www.google.com/maps/place/?q=place_id:" + df["place_id"]
@@ -48,13 +52,11 @@ def get_supermarkets(loc, search_string="supermarket", distance=miles_to_meter(2
         df["geometry"][1]["location"]["lat"],
         df["geometry"][1]["location"]["lng"],
     )
-    print(lat, lng)
     return lat, lng
 
 
-def get_completion(messages, model="gpt-3.5-turbo"):
+def get_completion(messages, model="gpt-3.5-turbo-0613"):
     """grabs completion"""
-    print(messages)
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
@@ -63,77 +65,73 @@ def get_completion(messages, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 
-st.set_page_config(layout="wide")
-st.title("Figure out a good name")
+def main():
+    """main entry point"""
+    st.set_page_config(layout="wide")
+    st.title("Figure out a good name")
 
-# Get user input using a text input field
-text = st.text_input("Enter your prompt here:")
-location = get_geolocation()
+    # Get user input using a text input field
+    st.session_state.text = st.text_input(
+        "Enter your prompt here:", st.session_state.text
+    )
+
+    st.session_state.location = get_geolocation()
+
+    # Check if the user has entered a prompt
+    if st.session_state.text:
+        # Display the prompt
+        st.write(f"You entered: {st.session_state.text}")
+
+        # Perform actions similar to your Flask app
+        # need to give a better prompt
+        prompt_answer = f"""
+        Perform the following actions:
+            1 - Look up all memories and summarize the list of ingredients
+            2 - Provide the name of a specific healthy dish that is popular in recipe books and list all the ingredients.
+            3 - Answer questions in the formation part
+            4 - For the `Recipe and Instructions` part, we want the formation to be:
+
+                Using the following format and only respond with this format:
+    
+                **Ingredients Summary:** <list of requirements>
+                **<find a dish name closely matching the name of the dish>**
+                **Estimated Cost:** <Estimated cost of this dish denoted in U.S. dollars>
+                **Estimated Time:** <Estimated time cost of this dish>
+                **Calorie:** <Estimated Calorie of this dish>
+                **Recipe and Instructions:** <>
+        ```{st.session_state.text}```
+        """
+
+        st.session_state.conversation_history.append(
+            {"role": "user", "content": prompt_answer}
+        )
+        response = get_completion(st.session_state.conversation_history)
+        st.session_state.conversation_history.append(
+            {"role": "assistant", "content": response}
+        )
+        # Display the response in the desired format
+        formatted_response = response.split("\n")
+        # Get the image URL using the 'dalle' function
+        image_url = dalle(response)
+
+        # Create three columns
+        if st.session_state.location:
+            col1, col2 = st.columns(2)
+
+            # Column 1 - Placeholder for some content
+            with col1:
+                # Display the image
+                st.image(image_url, use_column_width=True)
+
+            # Column 2 - Placeholder for the response from the prompt
+            with col2:
+                for line in formatted_response:
+                    st.markdown(line)
+
+                # Provide a download link
+                # Use st.experimental_singleton to prevent re-run
+                ste.download_button("Download recipe!", response, "dish-info.txt")
 
 
-# Check if the user has entered a prompt
-if text:
-    # Display the prompt
-    st.write(f"You entered: {text}")
-
-    # Perform actions similar to your Flask app
-    # need to give a better prompt
-    prompt_answer = f"""
-    Perform the following actions:
-        1 - Look up all memories and summarize the list of ingredients
-        2 - Provide the name of a specific healthy dish that is popular in recipe books and list all the ingredients.
-        3 - Answer questions in the formation part
-        4 - For the `Recipe and Instructions` part, we want the formation to be:
-
-        Using the following format and only respond with this format:
-        Ingredients Summary: <list of requirements>
-        <find a dish name closely matching the name of the dish>
-        Estimated Cost: <Estimated cost of this dish denoted in U.S. dollars>
-        Estimated Time: <Estimated time cost of this dish>
-        Calorie: <Estimated Calorie of this dish>
-        Recipe and Instructions: <>
-    ```{text}```
-    """
-
-    conversation_history.append({"role": "user", "content": prompt_answer})
-    response = get_completion(conversation_history)
-    conversation_history.append({"role": "assistant", "content": response})
-    # Display the response in the desired format
-    formatted_response = response.split("\n")
-    # Get the image URL using the 'dalle' function
-    image_url = dalle(response)
-
-    # Create three columns
-    if location:
-        col1, col2, col3 = st.columns(3)
-
-        # Column 1 - Placeholder for some content
-        with col1:
-            # Display the image
-            st.image(image_url, use_column_width=True)
-
-        # Column 2 - Placeholder for the response from the prompt
-        with col2:
-            for line in formatted_response:
-                st.markdown(line)
-
-        # Column 3 - Placeholder for some content
-        with col3:
-            print(location)
-            lat, long = location["coords"]["latitude"], location["coords"]["longitude"]
-            lat, lng = get_supermarkets((lat, long))
-            st.write("Best Supermarket")
-            m = folium.Map(location=[lat, lng], zoom_start=16)
-            st_data = st_folium(m, width=725)
-            # You can add any content you want in this column
-    else:
-        col1, col2 = st.columns(2)
-        # Column 1 - Placeholder for some content
-        with col1:
-            # Display the image
-            st.image(image_url, use_column_width=True)
-
-        # Column 2 - Placeholder for the response from the prompt
-        with col2:
-            for line in formatted_response:
-                st.markdown(line)
+if __name__ == "__main__":
+    main()
