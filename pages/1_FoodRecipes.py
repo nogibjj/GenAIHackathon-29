6b56f32ea82ef0e09b4f9de2f3b252f8182589d6
jeremymtan <jemtan@ucsc.edu>
2023-11-05 04:40:07 -0500

    fix

--- pages/1_FoodRecipes.py
+++ pages/1_FoodRecipes.py
@@ -15,8 +15,8 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-if "conversation_history" not in st.session_state:
-    st.session_state.conversation_history = []
+# Initialize conversation history as an empty list
+conversation_history = []
 
 if "text" not in st.session_state:
     st.session_state.text = ""
@@ -55,7 +55,7 @@ def get_supermarkets(loc, search_string="supermarket", distance=miles_to_meter(2
     return lat, lng
 
 
-def get_completion(messages, model="gpt-3.5-turbo-0613"):
+def get_completion(messages, model="gpt-3.5-turbo-16k"):
     """grabs completion"""
     response = openai.ChatCompletion.create(
         model=model,
@@ -86,33 +86,28 @@ def main():
         # need to give a better prompt
         prompt_answer = f"""
         Perform the following actions:
-            1 - Look up all memories and summarize the list of ingredients
-            2 - Provide the name of a specific healthy dish that is popular in recipe books and list all the ingredients.
-            3 - Answer questions in the formation part
-            4 - For the `Recipe and Instructions` part, we want the formation to be:
+            1 - Find the name of a specific healthy dish that is popular in recipe books and list all the ingredients.
+            2 - Answer questions in the formation part
+            3 - For the `Recipe and Instructions` part, we want the formation to be:
 
-                Using the following format and only respond with this format:
+                Using the following format and only respond with this format. You must give a recipe, you cannot say you cannot find one.:
     
-                **Ingredients Summary:** <list of requirements>
                 **<find a dish name closely matching the name of the dish>**
                 **Estimated Cost:** <Estimated cost of this dish denoted in U.S. dollars>
                 **Estimated Time:** <Estimated time cost of this dish>
                 **Calorie:** <Estimated Calorie of this dish>
                 **Recipe and Instructions:** <>
+                **Ingredients Summary:** <list of requirements based on recipe>
         ```{st.session_state.text}```
         """
 
-        st.session_state.conversation_history.append(
-            {"role": "user", "content": prompt_answer}
-        )
-        response = get_completion(st.session_state.conversation_history)
-        st.session_state.conversation_history.append(
-            {"role": "assistant", "content": response}
-        )
+        conversation_history.append({"role": "user", "content": prompt_answer})
+        response = get_completion(conversation_history)
+        conversation_history.append({"role": "assistant", "content": response})
         # Display the response in the desired format
         formatted_response = response.split("\n")
         # Get the image URL using the 'dalle' function
-        image_url = dalle(response)
+        image_url = dalle(formatted_response[0])
 
         # Create three columns
         if st.session_state.location:
@@ -130,7 +125,9 @@ def main():
 
                 # Provide a download link
                 # Use st.experimental_singleton to prevent re-run
-                ste.download_button("Download recipe!", response, "dish-info.txt")
+                ste.download_button(
+                    "Download recipe!", response, formatted_response[0] + ".txt"
+                )
 
 
 if __name__ == "__main__":
